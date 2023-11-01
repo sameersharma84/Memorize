2d83df9cec2b11b3670dfc169f06c2f886a94505
Sameer <sameersharma.dc@gmail.com>
2023-11-01 12:26:04 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sameer Sharma on 2023-11-01.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
